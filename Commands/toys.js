77901178c540f81f5d68d0bff38581877809eda4
Lee Pritchard <lee_pritchard@rocketmail.com>
2020-05-04 18:23:16 +0100

    v0.2
    Completed toys command, removed commands no longer needed

--- Commands/toys.js
+++ Commands/toys.js
@@ -15,26 +15,109 @@ module.exports = {
 	args: true,
 	execute(message, args) {
 		const embed = new Discord.MessageEmbed();
-		if (args[0] === `shaman`) {
+		if (args[0] === `shaman` | `sham`) {
 			embed.setColor(`BLUE`)
 			embed.setTitle(`Optimal Shaman Toys`)
 			embed.setDescription(`The Top 4 Toys for Shaman`)
             embed.addFields(
                 {name: `First`, value: `Sheep`},
-                {name: `Second`, value: `Snow Marmot`},
+                {name: `Second`, value: `Snow Marmot (keep regular too)`},
                 {name: `Third`, value: `Hermic Crab`},
                 {name: `Fourth`, value: `Ankylosaur`},
                 {name: `\u200B`, value: `\u200B`},
-                {name: `Optionals:`, value: `Irish Elk \nQueen Dragon\nWind Dragon`})
-        }
+                {name: `Optionals:`, value: `Irish Elk\nQueen Dragon\nWind Dragon`})
+		}
+		
+		if (args[0] === `druid`) {
+			embed.setColor(`GREEN`)
+			embed.setTitle(`Optimal Druid Toys`)
+			embed.setDescription(`The Top 4 Toys for Druid`)
+            embed.addFields(
+                {name: `First`, value: `Sheep`},
+                {name: `Second`, value: `Snow Marmot (keep regular too)`},
+                {name: `Third`, value: `Hermit Crab`},
+                {name: `Fourth`, value: `Moblin`},
+                {name: `\u200B`, value: `\u200B`},
+                {name: `Optionals:`, value: `Irish Elk\nQueen Dragon\nWind Dragon\nDodo Bird\nDandelion`})
+		}
             
-            
-            //Last Map, the Last Continent.
-            //What will you most “Likely Use” that normally a shaman doesn’t use. 
-            //1: Irish Elk 100% situational 
-            //2: Queen Dragon 50% Situational 
-            //3: Wind Dragon 10% small`)
+		if (args[0] === `warrior`) {
+			embed.setColor(`GREY`)
+			embed.setTitle(`Optimal Warrior Toys`)
+			embed.setDescription(`The Top 4 Toys for Warrior`)
+            embed.addFields(
+                {name: `First`, value: `White Camel`},
+                {name: `Second`, value: `Irish Elk`},
+                {name: `Third`, value: `Narwhal King`},
+                {name: `Fourth`, value: `Snow Marmot (keep regular too)`},
+                {name: `\u200B`, value: `\u200B`},
+                {name: `Optionals:`, value: `Dimetrodon\nTriceratops\nWalrus\nAnkylosaur\nWind Dragon\nDandelion`})
+		}
 
+		if (args[0] === `gladiator` | `glad`) {
+			embed.setColor(`DARK_RED`)
+			embed.setTitle(`Optimal Gladiator Toys`)
+			embed.setDescription(`The Top 4 Toys for Gladiator`)
+            embed.addFields(
+                {name: `First`, value: `Snow Marmot (keep regular too)`},
+                {name: `Second`, value: `White Camel`},
+                {name: `Third`, value: `Natwhal King`},
+                {name: `Fourth`, value: `Irish Elk`},
+                {name: `\u200B`, value: `\u200B`},
+                {name: `Optionals:`, value: `Dimetrodon\nTriceratops\nDandelion\nWind Dragon\nAnkylosaur`})
+		}
+
+		if (args[0] === `assassin` | `sin`) {
+			embed.setColor(`RED`)
+			embed.setTitle(`Optimal Assassin Toys`)
+			embed.setDescription(`The Top 4 Toys for Assassin`)
+            embed.addFields(
+                {name: `First`, value: `Snow Marmot (keep regular too)`},
+                {name: `Second`, value: `Black Moblin`},
+                {name: `Third`, value: `Sabre Tiger King`},
+                {name: `Fourth`, value: `Brown Bear`},
+                {name: `\u200B`, value: `\u200B`},
+                {name: `Optionals:`, value: `Anyklosaur\nUnicorn Gorilla\nYak King`})
+		}
+
+		if (args[0] === `hunter` | `hunt`) {
+			embed.setColor(`DARK_GREEN`)
+			embed.setTitle(`Optimal Hunter Toys`)
+			embed.setDescription(`The Top 4 Toys for Hunter`)
+            embed.addFields(
+                {name: `First`, value: `Black Moblin`},
+                {name: `Second`, value: `Brown Bear`},
+                {name: `Third`, value: `Sabre Tiger King`},
+                {name: `Fourth`, value: `Snowfield Marmot`},
+                {name: `\u200B`, value: `\u200B`},
+                {name: `Optionals:`, value: `Anyklosaur\nUnicorn Gorilla\nWarg King\nYak King`})
+		}
+
+		if (args[0] === `mage`) {
+			embed.setColor(`BLUE`)
+			embed.setTitle(`Optimal Mage Toys`)
+			embed.setDescription(`The Top 4 Toys for Mage`)
+            embed.addFields(
+                {name: `First`, value: `Snow Marmot (keep regular too)`},
+                {name: `Second`, value: `Black Moblin`},
+                {name: `Third`, value: `Sabre Tiger King`},
+                {name: `Fourth`, value: `Yak King`},
+                {name: `\u200B`, value: `\u200B`},
+                {name: `Optionals:`, value: `Brown Bear\nAnkylosaur\nUnicorn Gorilla\nWarg King`})
+		}
+
+		if (args[0] === `warlock` | `lock`) {
+			embed.setColor(`PURPLE`)
+			embed.setTitle(`Optimal Warlock Toys`)
+			embed.setDescription(`The Top 4 Toys for Warlock`)
+            embed.addFields(
+                {name: `First`, value: `Black Moblin`},
+                {name: `Second`, value: `Brown Bear`},
+                {name: `Third`, value: `Sabre Tiger King`},
+                {name: `Fourth`, value: `Snow Marmot (keep regular too)`},
+                {name: `\u200B`, value: `\u200B`},
+                {name: `Optionals:`, value: `Ankylosaur\nUnicorn Gorilla\nWarg King\nYak King`})
+		}
     message.channel.send(embed)
     },
 };
--- Commands/user.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: `user`,
-    description: `Information about the user`,
-    cooldown: 50,
-    execute(message, args) {
-        message.channel.send(`Your username is: ${message.author.username}\nYour ID: ${message.author.id}`);
-    },
-}
